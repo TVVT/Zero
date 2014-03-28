096182331b83ad969c42597a435f1917c0eeaade
wov <wovfeng@gmail.com>
2014-03-28 17:10:47 +0800

    add some style

--- public/manager/scripts/manager_page.js
+++ public/manager/scripts/manager_page.js
@@ -1,18 +1,6 @@
-(function(window) {
-	// var e, height;
-	// var e_iframe = document.querySelector("#page-manager-iframe"),
-	// 	moduleDataToString = document.getElementById("moduleDataToString"),
-	// 	watchSource = document.getElementById("watch-source"),
-	// 	iframeWrapper = document.getElementById("iframe-wrapper");
+(function(win) {
+	
 
-	// window.frames["page-manager-iframe"].onload = function() {
-	// 	e = window.frames["page-manager-iframe"].document.body;
-	// 	height = e.scrollHeight;
-	// 	e_iframe.style.height = height + "px";
-	// }
 
-	// //查看页面源代码
-	// watchSource.onclick = function(e) {
 
-	// }
 })(window);
