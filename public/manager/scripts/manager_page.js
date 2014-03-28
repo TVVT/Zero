9a2e029c88db905419c82ff6a02a4f471738eb78
kevin14 <kevin14@me.com>
2014-03-28 14:17:44 +0800

    增加了配置文件的显示

--- public/manager/scripts/manager_page.js
+++ public/manager/scripts/manager_page.js
@@ -1,24 +1,18 @@
 (function(window) {
-	var moduleName,developper,totolType,currType,e,e_moduleName,e_developper,e_currType,height;
-	var e_moduleName = document.querySelector(".manager-page-attr-name"),
-		e_developper = document.querySelector(".manager-page-attr-developper"),
-		managerModuleName = document.body.attributes.getNamedItem('manager-module-name').value,
-		e_iframe = document.querySelector("#page-manager-iframe");
+	var e, height;
+	var e_iframe = document.querySelector("#page-manager-iframe"),
+		moduleDataToString = document.getElementById("moduleDataToString"),
+		watchSource = document.getElementById("watch-source"),
+		iframeWrapper = document.getElementById("iframe-wrapper");
 
-		window.frames["page-manager-iframe"].onload = function() {
+	window.frames["page-manager-iframe"].onload = function() {
 		e = window.frames["page-manager-iframe"].document.body;
 		height = e.scrollHeight;
-		e_iframe.style.height = height+"px";
-		moduleName = e.attributes.getNamedItem('module-name');
-		developper = e.attributes.getNamedItem('developper');
+		e_iframe.style.height = height + "px";
+	}
 
-		//TODO 判断属性存在 如果不存在会出现错误
-		if(!moduleName||!developper) {
-			console.log("模块信息不完整！");
-			return;
-		};
+	//查看页面源代码
+	watchSource.onclick = function(e) {
 
-		// e_developper.innerText = developper.value;
-		// e_moduleName.innerText = moduleName.value;
 	}
 })(window);
