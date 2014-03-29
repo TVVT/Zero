e2eb65936d638abb1ace23cc312e910168131c34
wov <wovfeng@gmail.com>
2014-03-29 19:02:32 +0800

    add jquery & fix some style

--- public/manager/scripts/manager_page.js
+++ public/manager/scripts/manager_page.js
@@ -1,6 +1,14 @@
-(function(win) {
-	
+$(function(){
+	$('.showSource').on('click',function(){
+		$('.source-code-wrapper').toggleClass('show');
+	});
+
+	$('.source-code-wrapper .close').on('click',function(){
+		$('.source-code-wrapper').removeClass('show');
+	})
 
 
-
-})(window);
+	$('pre').on('click',function(){
+		// $(this).select();
+	})
+})
