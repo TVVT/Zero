fa70570e468a7c5e79f1281bdf7d81a50e62a224
wov <wovfeng@gmail.com>
2014-04-02 13:10:38 +0800

    restyle the qr code style

--- public/manager/scripts/manager_page.js
+++ public/manager/scripts/manager_page.js
@@ -1,50 +1,73 @@
 $(function() {
 
-	//二维码加载好之后开启webSocket
-	var qrCodeImg = document.getElementById('qrCodeImg');
-	qrCodeImg.onload = function() {
-		var ws = new WebSocket("ws://localhost:8081");
+    //二维码加载好之后开启webSocket
+    var qrCodeImg = document.getElementById('qrCodeImg');
+    qrCodeImg.onload = function() {
+        var ws = new WebSocket("ws://localhost:8081");
 
-		ws.onopen = function(e) {
-			console.log("连接成功。。。");
-			ws.send(JSON.stringify({
-				cid: $('#qrCodeImg').attr('qrCode')
-			}));
-		};
+        ws.onopen = function(e) {
+            console.log("连接成功。。。");
+            ws.send(JSON.stringify({
+                cid: $('#qrCodeImg').attr('qrCode')
+            }));
+        };
 
-		ws.onmessage = function(event) {
-			var data = JSON.parse(event.data);
-			if (data && data.status === 'ready') {
-				// qrCodeImg.style.display = 'none';
-				$('.page-feedback').css('display','block');
-				$('.user-agent').text(data['user-agent']);
-			};
-		}
+        ws.onmessage = function(event) {
+            var data = JSON.parse(event.data);
 
-	}
+            if (data && data.status === 'ready') {
+                var $div = $('<div class="feedback_box"></div>');
 
-	// $('#nav li').on('click', function(e) {
-	// 	$('.board').children().hide();
-	// 	$('.board .close').show();
-	// 	$('.board').toggleClass('show');
+                $div.html('你的手机环境为：');
+                var ua_span = $('<span>'+data['user-agent']+'</span>');
 
-	// 	switch (e.target.id.toString()) {
-	// 		case 'showQrCode':
-	// 			$('.board .qrCode').show();
-	// 			break;
-	// 		case 'showSource':
-	// 			$('.board .page-source').show();
-	// 			break;
-	// 		default:
-	// 	}
-	// })
+                var status = $('<div class="status"></div>');
+                var btn_ok = $('<a href="javascript:;" class="ok">一切ok</a>');
+                var btn_error = $('<a href="javascript:;" class="warning">反馈一个问题?</a>');
 
-	$('#showModules').on('click',function(){
-		$('.manager-page-modules').toggleClass('show');
-	})
+                status.append(btn_ok,'，',btn_error);
 
-	$('.board .close').on('click', function() {
-		$('.board').removeClass('show');
-	})
+                var $form = $('<div class="form">' +
+                                '<form>' +
+                                    '<textarea placeholder="请描述一下你的问题...."></textarea>' +
+                                    '<input type="submit" value="提交" />' +
+                                '</form>' +
+                            '</div>');
+
+                $div.append(ua_span,status,$form);
+
+                $('.page-feedback').append($div);
+
+                $div.addClass('show');
+
+                btn_ok.on('click',function(){
+                    status.html('thanks!');
+                    $form.remove();
+                });
+
+                btn_error.on('click',function(){
+                    $form.toggleClass('show');
+                    // todo useing ajax here..
+                });
+
+
+
+            };
+        }
+
+    }
+
+
+    $('#showQrCode').on('click',function(){
+        $('.manager-page-qrcode').toggleClass('show');
+    });
+
+    $('#showModules').on('click',function(){
+        $('.manager-page-modules').toggleClass('show');
+    });
+
+    $('.board .close').on('click', function() {
+        $('.board').removeClass('show');
+    });
 
 })
