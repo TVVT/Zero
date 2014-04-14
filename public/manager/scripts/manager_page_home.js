e61a40e2501bf88928d112abdd4dcb1dcfd8eac8
kevin14 <kevin14@me.com>
2014-04-14 18:19:28 +0800

    clean 3d matrix

--- public/manager/scripts/manager_page_home.js
+++ public/manager/scripts/manager_page_home.js
@@ -26,7 +26,7 @@ $(function() {
 		json.rotateY = index * 25;
 		left = index * 340;
 		_this.style['left'] = left + "px";
-		_this.style['-webkit-transform'] = "matrix3d(" + toMatrix3D(json) + ")";
+		// _this.style['-webkit-transform'] = "matrix3d(" + toMatrix3D(json) + ")";
 	})
 
 	$(list).scroll(function() {
@@ -34,11 +34,11 @@ $(function() {
 		console.log(stepWidth)
 		if (Math.ceil($(this).scrollLeft() / stepWidth) != prevP) {
 			prevP = Math.ceil($(this).scrollLeft() / stepWidth);
-			$(iframeWrappers).each(function(index) {
-				var _this = $(iframeWrappers)[index];
-				json.rotateY = -25*prevP + index * 25;
-				_this.style['-webkit-transform'] = "matrix3d(" + toMatrix3D(json) + ")";
-			})
+			// $(iframeWrappers).each(function(index) {
+			// 	var _this = $(iframeWrappers)[index];
+			// 	json.rotateY = -25*prevP + index * 25;
+			// 	_this.style['-webkit-transform'] = "matrix3d(" + toMatrix3D(json) + ")";
+			// })
 		}
 	})
 
