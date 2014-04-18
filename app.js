96fd6bef6e134c524dfa70abc3fadd0c7f0a2375
kevin14 <kevin14@me.com>
2014-04-18 12:55:24 +0800

    change fac

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.set('port', process.env.PORT || port);
 app.set('views', path.join(__dirname, '../Projects'));
 
 app.set('view engine', 'ejs');
-app.use(express.favicon());
 // app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
