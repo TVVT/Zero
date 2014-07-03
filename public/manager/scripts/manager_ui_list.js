666c803ff0e2cb1d3f42bf349509a529b1d3fb30
kevin14 <kevin14@me.com>
2014-07-03 09:54:39 +0800

    to pull

--- public/manager/scripts/manager_ui_list.js
+++ public/manager/scripts/manager_ui_list.js
@@ -1,3 +1,20 @@
 $(function() {
-	
+    var list_wrapper = $('.manager-ui-list');
+    list.forEach(function(cName, index) {
+        $.ajax({
+            method: "GET",
+            url: link + "/projects/"+projectName+"/components/"+cName+".json",
+            success: function(data) {
+            	var wrapper = $('<div class="mananger_ui_list_wrapper"></div>');
+            	var renderData = {};
+            	renderData.data = data;
+            	var html = new EJS({url: link + "/projects/"+projectName+"/components/"+cName+".ejs"}).render(renderData);
+            	wrapper.append($(html));
+            	list_wrapper.append(wrapper);
+            }
+        })
+    })
+
+
+
 })
