a19e14c29e1ed676b20cadb2fc0df9882fa7ce70
fangcao <836036143.qq.com>
2014-08-06 20:56:51 +0800

    zeroFrames

--- public/manager/scripts/manager_projects_list.js
+++ public/manager/scripts/manager_projects_list.js
@@ -1,27 +1,27 @@
 $(function() {
-	var collect=$('.manager_projects_list'),
-		iframeContent=$('.iframeContent');
-	/**
-	 * [收藏]
-	 * @param  {[type]} event [description]
-	 * @return {[type]}       [description]
-	 */
-	collect.on('click','.collect',function(event){
-		alert('aaaaa');
-		//debugger
-		event.stopPropagation();
-		event.preventDefault();
-	});
+    var collect = $('.manager_projects_list'),
+        iframeContent = $('.iframeContent');
+    /**
+     * [收藏]
+     * @param  {[type]} event [description]
+     * @return {[type]}       [description]
+     */
+    collect.on('click', '.collect', function(event) {
+        alert('aaaaa');
+        //debugger
+        event.stopPropagation();
+        event.preventDefault();
+    });
 
-	collect.on('click','img',function(event){
-		var a=$(this).attr('data-url'),
-			ifr=iframeContent.find('iframe');
+    collect.on('click', 'img', function(event) {
+        var a = $(this).attr('data-url'),
+            ifr = iframeContent.find('iframe');
 
-		ifr.attr('src',a);
-		iframeContent.addClass('show');
-	});
+        ifr.attr('src', a);
+        iframeContent.addClass('show');
+    });
+    iframeContent.on('click', '.close', function() {
+        iframeContent.removeClass('show');
+    });
 
-	/*iframeContent.on('mouseleave',function(){
-		iframeContent.removeClass('show');
-	});*/
 });
