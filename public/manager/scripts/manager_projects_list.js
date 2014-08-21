9dc4d1472206bc285de907d44a7bacabd547c5b6
fangcao <836036143.qq.com>
2014-08-21 16:56:40 +0800

    zeroBUI

--- public/manager/scripts/manager_projects_list.js
+++ public/manager/scripts/manager_projects_list.js
@@ -4,6 +4,7 @@ $(function() {
         manager_close = $('.manager_close'),
         isLocalStorage = window.localStorage ? true : false,
         projectList = new Array();
+    var iframeUrl;
 
     (function() {
         if (!localStorage.getItem('projectList')) {
@@ -125,40 +126,73 @@ $(function() {
             localStorage.setItem('projectList', projectList);
         }
     });
+
+    /**
+     * [openIframeContent 打开弹出]
+     * @param  {[String]} url [iframe 的 url]
+     */
+
+
+    function openIframeContent(url) {
+        var ifr = iframeContent.find('iframe');
+
+        if (!iframeContent.hasClass('show')) {
+            iframeContent.addClass('show');
+            collect.addClass('show');
+        } else {
+            IframeUrlFun(ifr);
+        }
+    }
+    /**
+     * [IframeUrlFun 给iframeUrl赋值]
+     * @param {[type]} obj [iframe对象]
+     */
+    function IframeUrlFun(obj) {
+        obj.attr('src', iframeUrl).css({
+            'height': iframeContent.height() + 'px'
+        });
+    }
+    /**
+     * [IframeShow 找到iframe看有没有show，有就添加src]
+     */
+    function IframeShow() {
+        var ifr = iframeContent.find('iframe');
+        if (this.classList.contains('show')) {
+            IframeUrlFun(ifr);
+        }
+    }
+
     /**
      * [内容页的控制]
      * @param  {[type]} event [description]
      * @return {[type]}       [description]
      */
     collect.on('click', 'img', function(event) {
-        var a = $(this).attr('data-url'),
-            ifr = iframeContent.find('iframe');
-
-        ifr.attr('src', a).css({
-            'height': iframeContent.height() + 'px'
-        });
-        if (iframeContent.hasClass('show')) {
-            return;
-        }
-        iframeContent.addClass('show');
-        collect.addClass('show');
+        var url = $(this).attr('data-url');
+        iframeUrl = url;
+        openIframeContent(url);
     });
+    
+    iframeContent[0].addEventListener('webkitTransitionEnd', IframeShow, function() {
+        IframeShow();
+    }, false);
+
 
     iframeContent.on('click', 'i', function() {
         if (collect.hasClass('show')) {
             iframeContent.removeClass('show');
             collect.removeClass('show').removeClass('hide');
-            iframeContent.find('iframe').attr('src','');
-        } 
+            iframeContent.find('iframe').attr('src', '');
+        }
     });
 
     manager_close.on('click', 'i', function() {
-        if(iframeContent.find('iframe').attr('src')===''){
+        if (iframeContent.find('iframe').attr('src') === '') {
             return;
         }
-        if(collect.hasClass('hide')){
+        if (collect.hasClass('hide')) {
             collect.removeClass('hide');
-        }else{
+        } else {
             collect.addClass('hide');
         }
     });
