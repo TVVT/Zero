9000fb6e6fc479446aa3e534aad3eaf69931360d
luckyrobin <luckyrobin@github.com>
2015-05-08 16:40:06 +0800

    centos pgeep

--- routes/svn.js
+++ routes/svn.js
@@ -1,14 +1,15 @@
 var exec = require('child_process').exec;
 
 exports.svnup = function(req,res){
-    var testcmd = 'pgrep -l svn';
+    var testcmd = 'pgrep svn';
     // 获取svn更新路径，如果没有更新路径则默认为全局
     var cmd = 'cd /home/whtest/server/tvvt && svn up';
+	console.log(cmd);
     exec(testcmd,function(err,stdout,stderr){
-        if(stdout||err||stderr){
-            res.status(500).end('其他同事更新svn中....去群里吼下吧！');
+        if(stdout){
+           res.status(500).end('其他同事更新svn中....去群里吼下吧！');
         }else{
-            exec(cmd, function(err, stdout, stderr) {
+           exec(cmd, function(err, stdout, stderr) {
                 if (err) {
                     res.status(500).end(err.toString());
                 } else {
